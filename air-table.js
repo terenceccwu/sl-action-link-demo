0d7bb4f3e298088c0f7f689ab4e0801b76c551ef
Terence Wu <terence@shoplineapp.com>
2022-03-24 13:10:05 +0800

    remove api key

--- air-table.js
+++ air-table.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch')
 
 const AIR_TABLE_ENDPOINT = 'https://api.airtable.com/v0/appMKnf5itvZqBkez';
-const TOKEN = 'keyMPDDzP87h2mavm';
+const TOKEN = process.env.AIR_TABLE_API_KEY || '';
 
 async function request(method, url) {
   const response = await fetch(`${AIR_TABLE_ENDPOINT}${url}`, {
